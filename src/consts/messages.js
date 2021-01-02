6864caf1a6ddf207db7d5902723479921cc916d2
Thami Memel <memelthami@gmail.com>
2021-01-03 00:53:55 +0100

    user registration success

---
+++ src/consts/messages.js
@@ -0,0 +1,8 @@
+const USER_REGISTRED_SUCCESSFULLY = {
+  msg:
+    "you have registered successfully please check your email to activate your account",
+};
+
+module.exports = {
+  USER_REGISTRED_SUCCESSFULLY,
+};
