ba08d41295a8c136f74dafd2f3ece9ab4da6a4c4
Thami Memel <memelthami@gmail.com>
2021-01-02 23:35:28 +0100

    add generate email verification token func

--- tst.js
+++
@@ -1,8 +0,0 @@
-const bcrypt = require("bcrypt");
-
-const crypt = async (password, rounds) => {
-  const hashed = await bcrypt.hash(password, rounds);
-  console.log(hashed);
-};
-
-crypt("811998aZ-", 10);
