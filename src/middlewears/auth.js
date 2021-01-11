6a226d9933222855bf9007e6b903d663cebad0a5
Thami Memel <memelthami@gmail.com>
2021-01-11 13:51:30 +0100

    user auth middlewear

--- src/middlewears/auth.js
+++ src/middlewears/auth.js
@@ -25,6 +25,30 @@ const authAdmin = async (req, res, next) => {
   }
 };
 
+const authUser = async (req, res, next) => {
+  try {
+    // verify if token exists
+    const token = req.header("x-auth-token");
+    if (!token) {
+      return res.status(400).json(INVALID_TOKEN);
+    }
+
+    // Decode JWT from token
+    try {
+      const decoded = jwt.decode(token, config.get("jwtKey"));
+      // Verrify the id is correct
+      req.user = decoded.user;
+      next();
+    } catch (e) {
+      return res.status(400).json(INVALID_TOKEN);
+    }
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).json(INTERNAL_SERVER_ERROR);
+  }
+};
+
 module.exports = {
   authAdmin,
+  authUser,
 };
