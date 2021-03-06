75eb6863fa36af045248635c7efd4c159ad6a5e2
Thami Memel <memelthami@gmail.com>
2021-03-06 14:52:48 +0100

    added MESSAGE_SENT_SUCCESSFULLY

--- src/consts/messages.js
+++ src/consts/messages.js
@@ -11,8 +11,13 @@ const USER_CREATED_SUCCESSFULLY = {
   msg: "user created successfully",
 };
 
+const MESSAGE_SENT_SUCCESSFULLY = {
+  msg: "message sent successfully",
+};
+
 module.exports = {
   USER_REGISTRED_SUCCESSFULLY,
   EMAIL_CONFIRMED_SUCCESSFULLY,
   USER_CREATED_SUCCESSFULLY,
+  MESSAGE_SENT_SUCCESSFULLY,
 };
