830284c66c69d3bfddb0be9944b9cb996a6e8aea
Thami Memel <memelthami@gmail.com>
2021-03-25 15:08:38 +0100

    remove unused comments route

--- src/server.js
+++ src/server.js
@@ -19,7 +19,6 @@ app.use("/api/v1/auth", require("./routes/api/v1/auth"));
 app.use("/api/v1/admins", require("./routes/api/v1/admins"));
 app.use("/api/v1/users", require("./routes/api/v1/users"));
 app.use("/api/v1/questions", require("./routes/api/v1/questions"));
-app.use("/api/v1/comments", require("./routes/api/v1/comments"));
 
 app.listen(PORT, () => {
   console.log(`Server started: http://localhost:3000`);
